7606a8ea360593a26e852c8a23430b90a70a31d7
diloes <diloes@hotmail.com>
2021-05-31 18:54:24 +0200

    finish

--- src/js/app.js
+++ src/js/app.js
@@ -4,4 +4,10 @@ const menu = document.querySelector('.header__hamburguer');
 
 menu.addEventListener("click", function() {
     document.querySelector('.nav__js').classList.add('nav__hamburguer');
+
+    const aspa = document.querySelector('.logo__cerrar');
+    aspa.addEventListener('click', () => {
+        document.querySelector('.nav__js').classList.remove('nav__hamburguer');
+    })
+
   });
